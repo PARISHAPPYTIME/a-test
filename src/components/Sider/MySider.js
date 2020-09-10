9a8f62919d82dfe9c69f4d5ef8243fc0e6785b47
杨东平 <18267094443@163.com>
2020-09-10 20:13:27 +0800

    添加代码展示效果

--- src/components/Sider/MySider.js
+++ src/components/Sider/MySider.js
@@ -1,131 +1,150 @@
-import React from 'react'
-import { Menu } from 'antd'
-import './MySider.less'
-import { Link } from 'react-router-dom'
+import React from "react"
+import { Menu } from "antd"
+import "./MySider.less"
+import { Link } from "react-router-dom"
 
-import arrayToTree from 'array-to-tree'
-import axios from 'axios'
-import NProgress from 'nprogress'
+import arrayToTree from "array-to-tree"
+import axios from "axios"
+import NProgress from "nprogress"
 
 import {
-  // 	// 	AppstoreOutlined,
-  // PieChartOutlined,
-  PlusOutlined,
-  // 	// 	DesktopOutlined,
-  ContainerOutlined,
-  MailOutlined,
-} from '@ant-design/icons'
+	// 	// 	AppstoreOutlined,
+	// PieChartOutlined,
+	PlusOutlined,
+	// 	// 	DesktopOutlined,
+	ContainerOutlined,
+	MailOutlined,
+} from "@ant-design/icons"
 
 const { SubMenu } = Menu
 
 class MySider extends React.Component {
-  state = {
-    mode: 'inline',
-    theme: 'light',
-    collapsed: false,
-    list: [],
-  }
+	state = {
+		mode: "inline",
+		theme: "light",
+		collapsed: false,
+		list: [],
+	}
 
-  changeMode = (value) => {
-    this.setState({
-      mode: value ? 'vertical' : 'inline',
-    })
-  }
+	changeMode = (value) => {
+		this.setState({
+			mode: value ? "vertical" : "inline",
+		})
+	}
 
-  changeTheme = (value) => {
-    this.setState({
-      theme: value ? 'dark' : 'light',
-    })
-  }
+	changeTheme = (value) => {
+		this.setState({
+			theme: value ? "dark" : "light",
+		})
+	}
 
-  matchMap = new Map([['<PlusOutlined />', <PlusOutlined />]])
+	matchMap = new Map([["<PlusOutlined />", <PlusOutlined />]])
 
-  menuList = (obj) => {
-    if (!obj) return
-    if (!obj.children || obj.children.length <= 1) {
-      return (
-        <Menu.Item key={obj.id} icon={this.matchMap.get(obj.icon)}>
-          <Link to={obj.href}>{obj.name}</Link>
-        </Menu.Item>
-      )
-    } else {
-      return (
-        <SubMenu
-          key={obj.id}
-          title={
-            <span>
-              <MailOutlined />
-              <span>{obj.name}</span>
-            </span>
-          }
-        >
-          {obj.children.map((item) => {
-            if (item.children) {
-              return this.menuList(item)
-            } else {
-              return (
-                <Menu.Item key={item.id} icon={<ContainerOutlined />}>
-                  <Link to={item.href}>{item.name}</Link>
-                </Menu.Item>
-              )
-            }
-          })}
-        </SubMenu>
-      )
-    }
-  }
+	onOpenChange = (openKeys) => {
+		const latestOpenKey = openKeys.find(
+			(key) => this.state.openKeys.indexOf(key) === -1
+		)
+		if (this.rootSubmenuKeys.indexOf(latestOpenKey) === -1) {
+			this.setState({ openKeys })
+		} else {
+			this.setState({
+				openKeys: latestOpenKey ? [latestOpenKey] : [],
+			})
+		}
+	}
 
-  render() {
-    console.log(this.state.list)
-    return (
-      <div className="sider-container">
-        <Menu
-          defaultSelectedKeys={['1']}
-          defaultOpenKeys={['1']}
-          mode="inline"
-          theme={this.state.theme}
-          inlineCollapsed={this.state.collapsed}
-        >
-          {this.state.list[0] &&
-            this.state.list[0].children.map((item) => {
-              return this.menuList(item)
-            })}
-        </Menu>
-      </div>
-    )
-  }
+	bindClick = (e) => {
+		console.log(e.key)
+		axios({ url: `http://192.168.0.57:3000/v2/code/${e.key}` }).then((res) => {
+			// console.log(res)
+			this.setState({})
+		})
+	}
 
-  arrayToTree = (list) => {
-    const newList = list.map((item) => {
-      return {
-        ...item,
-        path: item.href,
-        colName: item.name,
-      }
-    })
-    return arrayToTree(newList, {
-      parentProperty: 'parentId',
-      customID: 'id',
-    })
-  }
+	menuList = (obj) => {
+		if (!obj) return
+		if (!obj.children || obj.children.length <= 1) {
+			return (
+				<Menu.Item key={obj.id} icon={this.matchMap.get(obj.icon)}>
+					<Link to={`/container/${obj.id}`}>{obj.name}</Link>
+				</Menu.Item>
+			)
+		} else {
+			return (
+				<SubMenu
+					key={obj.id}
+					title={
+						<span>
+							<MailOutlined />
+							<span>{obj.name}</span>
+						</span>
+					}
+				>
+					{obj.children.map((item) => {
+						if (item.children) {
+							return this.menuList(item)
+						} else {
+							return (
+								<Menu.Item key={item.id} icon={<ContainerOutlined />}>
+									<Link to="/container/">{item.name}</Link>
+								</Menu.Item>
+							)
+						}
+					})}
+				</SubMenu>
+			)
+		}
+	}
 
-  componentDidMount() {
-    console.log(1)
-    NProgress.start()
-    axios({
-      url: 'http://192.168.0.57:3000/api/a/data/getMenuList',
-    })
-      .then((res) => {
-        this.setState({
-          list: this.arrayToTree(res.data.data),
-        })
-      })
-      .finally(() => {
-        setTimeout(() => {
-          NProgress.done(true)
-        }, 2000)
-      })
-  }
+	render() {
+		return (
+			<div className="sider-container">
+				<Menu
+					defaultSelectedKeys={["3123123123"]}
+					defaultOpenKeys={["1"]}
+					onOpenChange={this.onOpenChange}
+					mode="inline"
+					theme={this.state.theme}
+					inlineCollapsed={this.state.collapsed}
+					onClick={this.bindClick}
+				>
+					{this.state.list[0] &&
+						this.state.list[0].children.map((item) => {
+							return this.menuList(item)
+						})}
+				</Menu>
+			</div>
+		)
+	}
+
+	arrayToTree = (list) => {
+		const newList = list.map((item) => {
+			return {
+				...item,
+				path: item.href,
+				colName: item.name,
+			}
+		})
+		return arrayToTree(newList, {
+			parentProperty: "parentId",
+			customID: "id",
+		})
+	}
+
+	componentDidMount() {
+		NProgress.start()
+		axios({
+			url: "http://192.168.0.57:3000/api/a/data/getMenuList",
+		})
+			.then((res) => {
+				this.setState({
+					list: this.arrayToTree(res.data.data),
+				})
+			})
+			.finally(() => {
+				NProgress.done(true)
+			})
+	}
 }
 
 export default MySider
--- src/views/code/index.js
+++ src/views/code/index.js
@@ -1,37 +1,212 @@
-import React from 'react'
-import Highlight from 'react-highlight'
-import './index.less'
-import Axios from 'axios'
-import AddCodeForm from './add'
+import React from "react"
+import Highlight from "react-highlight"
+import "./index.less"
+import Axios from "axios"
+import {
+	Button,
+	Modal,
+	Form,
+	Input,
+	Upload,
+	message,
+	Select,
+	Divider,
+	Avatar,
+} from "antd"
+import { UploadOutlined, CopyOutlined } from "@ant-design/icons"
 
+import copy from "copy-to-clipboard"
+
+let fileid = ""
+
+const props = {
+	name: "wenjian",
+	action: "http://192.168.0.57:3000/file/Node",
+	headers: {
+		authorization: "authorization-text",
+	},
+	onChange(info) {
+		if (info.file.status !== "uploading") {
+			console.log(info.file, info.fileList)
+			fileid = info.file.response.fileId
+		}
+		if (info.file.status === "done") {
+			message.success(`${info.file.name} file uploaded successfully`)
+		} else if (info.file.status === "error") {
+			message.error(`${info.file.name} file upload failed.`)
+		}
+	},
+}
+
+// console.log(Form.useForm())
 class PageCode extends React.Component {
-  state = {
-    language: 'javascript',
-    content: '',
-  }
-  render() {
-    return (
-      <div className="app-code">
-        <Highlight language={this.state.language}>
-          {this.state.content}
-        </Highlight>
-        <AddCodeForm />
-      </div>
-    )
-  }
+	state = {
+		language: "javascript",
+		content: "function",
+		modal2Visible: false,
+		loading: false,
 
-  componentDidMount() {
-    Axios.get('http://localhost:3010/v2/Node/node.js')
-      .then((res) => {
-        console.log(res)
-        this.setState({
-          content: res.data.data,
-        })
-      })
-      .finally(() => {
-        console.log('结束')
-      })
-  }
+		title: "",
+		type: "",
+	}
+	formRef = React.createRef()
+	copyInput = React.createRef()
+
+	setModal2Visible(modal2Visible) {
+		this.setState({ modal2Visible })
+	}
+	onFinish = (values) => {
+		console.log("Success:", values)
+	}
+
+	onFinishFailed = (errorInfo) => {
+		console.log("Failed:", errorInfo)
+	}
+
+	onGenderChange = (value) => {
+		console.log(this.formRef)
+		this.setState({
+			type: value,
+		})
+	}
+	layout = {
+		labelCol: { span: 6 },
+		wrapperCol: { span: 18 },
+	}
+	setUploadFile = () => {
+		console.log({
+			fileid: fileid,
+			title: this.state.title,
+			type: this.state.type,
+		})
+		Axios.post("http://192.168.0.57:3000/api/a/code/saveCode", {
+			fileid: fileid,
+			title: this.state.title,
+			type: this.state.type,
+		}).then((res) => {})
+	}
+	bindOnChange = (e) => {
+		this.setState({
+			[e.target.name]: e.target.value,
+		})
+	}
+
+	copy = () => {
+		// 粘贴输入框中的内容
+		copy(this.state.content)
+		message.success("复制成功，如果失败，请在输入框内手动复制.")
+	}
+
+	render() {
+		return (
+			<div className="app-code">
+				<div className="code-controls-box">
+					<Avatar
+						style={{ backgroundColor: "#ffbf00", verticalAlign: "middle" }}
+						size="large"
+						gap={4}
+					>
+						U
+					</Avatar>
+					<div className="kong"></div>
+					<Button type="link">
+						<CopyOutlined />
+						编辑
+					</Button>
+					<Divider type="vertical" />
+					<Button type="link" onClick={this.copy}>
+						<CopyOutlined />
+						复制
+					</Button>
+				</div>
+				<Highlight className={this.state.language} ref={this.copyInput}>
+					{this.state.content}
+				</Highlight>
+
+				<Button type="link" onClick={() => this.setModal2Visible(true)}>
+					添加内容
+				</Button>
+				<Modal
+					title="Vertically centered modal dialog"
+					centered
+					visible={this.state.modal2Visible}
+					onOk={() => this.setModal2Visible(false)}
+					onCancel={() => this.setModal2Visible(false)}
+					footer={[
+						<Button key="back" onClick={() => this.setModal2Visible(false)}>
+							Return
+						</Button>,
+						<Button
+							key="submit"
+							type="primary"
+							loading={this.state.loading}
+							onClick={() => this.setUploadFile(false)}
+						>
+							Submit
+						</Button>,
+					]}
+				>
+					<Form
+						name="basic"
+						{...this.layout}
+						initialValues={{ remember: true }}
+						onFinish={this.onFinish}
+						onFinishFailed={this.onFinishFailed}
+						ref={this.formRef}
+					>
+						<Form.Item
+							label="标签标题"
+							name="username"
+							rules={[
+								{ required: true, message: "Please input your username!" },
+							]}
+						>
+							<Input
+								name="title"
+								value={this.state.title}
+								onChange={this.bindOnChange}
+							/>
+						</Form.Item>
+						<Form.Item
+							label="上传文件"
+							rules={[{ required: true, message: "Please   your username!" }]}
+						>
+							<Upload {...props}>
+								<Button icon={<UploadOutlined />}>Click to Upload</Button>
+							</Upload>
+						</Form.Item>
+						<Form.Item
+							name="type"
+							label="代码块的类型"
+							rules={[{ required: true }]}
+						>
+							<Select
+								placeholder="Select a option and change input text above"
+								onChange={this.onGenderChange}
+								allowClear
+							>
+								<Select.Option value="male">male</Select.Option>
+								<Select.Option value="female">female</Select.Option>
+								<Select.Option value="other">other</Select.Option>
+							</Select>
+						</Form.Item>
+					</Form>
+				</Modal>
+			</div>
+		)
+	}
+
+	componentDidMount() {
+		Axios.get("http://192.168.0.57:3000/v2/Node/node.js")
+			.then((res) => {
+				this.setState({
+					content: res.data.data,
+				})
+			})
+			.finally(() => {
+				console.log("结束")
+			})
+	}
 }
 
 export default PageCode
